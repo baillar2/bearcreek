48ee172228a0d749dad889aabed7283d0c272bbd
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-02-08 12:24:08 -0700

    carousel and admin started

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 angular.module('moduleOne',[])
 
 angular.module('moduleOne')
-	.controller('controllerOne', ['$scope',function($scope){
+	.controller('controllerOne', ['$scope', 'content', function($scope, content){
+		
+
 
 	}])
+angular.module('moduleOne')
+	.controller('adminController', ['$scope', 'content', function($scope, content, ){
+
+
+
+
+	}])
+
+angular.module('moduleOne')
+	.factory('content', [function(){
+		$scope.gallery = []
+		$scope.blogs = []
+		$scope.testimonials = []
+		return {
+			gallery		: 	$scope.gallery,
+			blogs		: 	$scope.blogs,
+			testimonials: 	$scope.testimonials
+		}
+	}])		
+
