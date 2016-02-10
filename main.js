1ad5b18fe33e8163815b163e82aaa7451f4ca00b
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-02-09 17:05:40 -0700

    need to finish nav bar

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 angular.module('moduleOne',[])
 
 angular.module('moduleOne')
+	.controller('navController', ['$scope', function($scope){
+			$scope.aboutUs = true
+		$scope.aboutLink = function(){
+			$scope.aboutUs = true
+			$scope.services = false
+			$scope.blog = false
+			$scope.contactUs = false
+		}
+		$scope.servicesLink = function(){
+			$scope.aboutUs = false
+			$scope.services = true
+			$scope.blog = false
+			$scope.contactUs = false
+			console.log('click fired')	
+		}
+		$scope.blogLink = function(){
+			$scope.aboutUs = false
+			$scope.services = false
+			$scope.blog = true
+			$scope.contactUs = false	
+		}
+		$scope.contactLink = function(){
+			$scope.aboutUs = false
+			$scope.services = false
+			$scope.blog = false
+			$scope.contactUs = true	
+		}
+	}])
+angular.module('moduleOne')
 	.controller('controllerOne', ['$scope', 'content', function($scope, content){
 	   	$scope.gallery = content.gallery
 		$scope.blogs = content.blogs
 		$scope.testimonials = content.testimonials
-
+		$scope.aboutUs = true
 
 	}])
 angular.module('moduleOne')
@@ -50,10 +79,10 @@ angular.module('moduleOne')
 
 angular.module('moduleOne')
 	.factory('content', [ function(){
-		var gallery = [{image:"images/stock-1.jpg"}, 
-		{image:"images/stock-2.jpg"}, 
-		{image:"images/stock-3.jpg"},
-		{image:"images/stock-4.jpg"}]
+		var gallery = [{image:"images/stock1.jpg"}, 
+		{image:"images/stock2.jpg"}, 
+		{image:"images/stock3.png"},
+		{image:"images/stock4.jpg"}]
 		var blogs = []
 		var testimonials = []
 		return {
